413e51e441e631b585c9a415e19b89cfda1255fc
Daniel Juventude <deljuven@ymail.com>
2024-06-09 20:57:32 -0300

    adição das novas tabelas loja e tipo_loja

--- START/01_ddl.sql
+++ START/01_ddl.sql
@@ -67,4 +67,34 @@ create table sistema_controle.produto (
 );
 comment on table sistema_controle.produto is 'PRODUTO de um ITEM-BASE na tal MARCA';
 
+create table sistema_controle.loja (
+	id serial,
+	nome varchar(100) not null,
+	nome_unique varchar(100) not null,
+	apelido varchar(100) not null,
+	rua varchar(200) not null,
+	numero varchar(10) not null default 'n/a',
+	cep varchar(20) null,
+	bairro varchar(50) not null,
+	cidade varchar(50) not null,
+	criado_em timestamptz default now(),
+	atualizado_em timestamptz null,
+	desativado_em timestamptz null,
+	constraint loja_id_pkey primary key(id),
+	constraint loja_nu_de_uniq unique(nome_unique, desativado_em)
+);
+comment on table sistema_controle.loja is 'LOJA onde é feito as compras';
+
+create table sistema_controle.tipo_loja (
+	id serial,
+	nome varchar(100) not null,
+	nome_unique varchar(100) not null,
+	descricao varchar(200) null,
+	criado_em timestamptz default now(),
+	atualizado_em timestamptz null,
+	desativado_em timestamptz null,
+	constraint tipo_loja_id_pkey primary key(id),
+	constraint tipo_loja_nu_de_uniq unique(nome_unique, desativado_em)
+);
+
 commit;
--- UPDATE/01_ddl.sql
+++ UPDATE/01_ddl.sql
@@ -1,9 +1,33 @@
-
 begin;
-alter table sistema_controle.item_base
-drop constraint item_base_n_unq;
 
-alter table sistema_controle.item_base
-add constraint item_base_n_unq_de unique (nome_unique, desativado_em);
+create table sistema_controle.loja (
+	id serial,
+	nome varchar(100) not null,
+	nome_unique varchar(100) not null,
+	apelido varchar(100) not null,
+	rua varchar(200) not null,
+	numero varchar(10) not null default 'n/a',
+	cep varchar(20) null,
+	bairro varchar(50) not null,
+	cidade varchar(50) not null,
+	criado_em timestamptz default now(),
+	atualizado_em timestamptz null,
+	desativado_em timestamptz null,
+	constraint loja_id_pkey primary key(id),
+	constraint loja_nu_de_uniq unique(nome_unique, desativado_em)
+);
+comment on table sistema_controle.loja is 'LOJA onde é feito as compras';
+
+create table sistema_controle.tipo_loja (
+	id serial,
+	nome varchar(100) not null,
+	nome_unique varchar(100) not null,
+	descricao varchar(200) null,
+	criado_em timestamptz default now(),
+	atualizado_em timestamptz null,
+	desativado_em timestamptz null,
+	constraint tipo_loja_id_pkey primary key(id),
+	constraint tipo_loja_nu_de_uniq unique(nome_unique, desativado_em)
+);
 
 commit;
