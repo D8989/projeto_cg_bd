0f0896354279504ad1a267d37d10d86401605d5c
Daniel Juventude <deljuven@ymail.com>
2024-06-10 23:42:26 -0300

    adição da chave estrangeira tipo-loja na tabela loja

--- UPDATE/01_ddl.sql
+++ UPDATE/01_ddl.sql
@@ -5,6 +5,7 @@ create table sistema_controle.loja (
 	nome varchar(100) not null,
 	nome_unique varchar(100) not null,
 	apelido varchar(100) not null,
+	tipo_loja_id integer not null,
 	rua varchar(200) not null,
 	numero varchar(10) not null default 'n/a',
 	cep varchar(20) not null default '00000000',
@@ -15,6 +16,10 @@ create table sistema_controle.loja (
 	atualizado_em timestamptz null,
 	desativado_em timestamptz null,
 	constraint loja_id_pkey primary key(id),
+	constraint loja_id_tl_id_fkey foreign key(tipo_loja_id)
+		references sistema_controle.tipo_loja(id)
+		on update cascade
+		on delete no action,
 	constraint loja_nu_de_uniq unique(nome_unique, desativado_em)
 );
 comment on table sistema_controle.loja is 'LOJA onde é feito as compras';
