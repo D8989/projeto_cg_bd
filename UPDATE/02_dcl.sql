3c5fc1be5b8422e0bf1ca3fe3bbc0c899a313632
Daniel Juventude <deljuven@ymail.com>
2024-06-13 23:34:55 -0300

    grant das novas tabelas

--- UPDATE/02_dcl.sql
+++ UPDATE/02_dcl.sql
@@ -1,6 +1,6 @@
 begin;
 
-grant all privileges on table sistema_controle.loja, sistema_controle.tipo_loja to usuario_api;
+grant all privileges on table sistema_controle.compra, sistema_controle.usuario, sistema_controle.pagamento, sistema_controle.item_compra to usuario_api;
 grant all privileges on all sequences in schema sistema_controle to usuario_api;
 grant all privileges on all functions in schema sistema_controle to usuario_api;
 grant all privileges on all procedures in schema sistema_controle to usuario_api;
